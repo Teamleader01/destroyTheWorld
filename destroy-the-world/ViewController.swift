9a2d9905243d6328aa1ebc15b4e6fd35c35f21f4
Kevin De wit <kevin.de.wit@telenet.be>
2018-10-14 18:29:37 +0200

    Initial Commit

---
+++ destroy-the-world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  destroy-the-world
+//
+//  Created by Kevin De wit on 14/10/2018.
+//  Copyright © 2018 Kevin De wit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
